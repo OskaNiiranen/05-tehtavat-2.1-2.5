c7c8ac2698b6abd2d34c06f56ac05e1d34e66323
OskaNiiranen <oska.niiranen@gmail.com>
2023-10-27 10:52:00 +0300

    2.5: erillinen moduuli

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Course from "./Course";
 
 const App = () => {
   const courses = [
@@ -50,45 +51,6 @@ const App = () => {
     <div>
       {courses.map((course) => (
         <Course key={course.id} course={course} />
-      ))}
-    </div>
-  );
-};
-
-const Course = ({ course }) => {
-  const totalExercises = course.parts.reduce(
-    (sum, part) => sum + part.exercises,
-    0
-  );
-
-  return (
-    <div>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <p>
-        <strong>Total of {totalExercises} exercises</strong>
-      </p>
-    </div>
-  );
-};
-
-const Header = ({ name }) => {
-  return <h2>{name}</h2>;
-};
-
-const Part = ({ part }) => {
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>
-  );
-};
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map((part) => (
-        <Part key={part.id} part={part} />
       ))}
     </div>
   );
